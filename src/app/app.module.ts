9c36d120faef25809896e1e5c8a1534dc02c2476
xmb19 <muratbektas019@gmail.com>
2025-01-30 16:10:21 +0300

    30-01

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './components/product/product.component';
 import { CategoryComponent } from './components/category/category.component';
 import { NaviComponent } from './components/navi/navi.component';
+import { provideHttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -16,10 +18,12 @@ import { NaviComponent } from './components/navi/navi.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    
   ],
   providers: [
-    provideClientHydration(withEventReplay())
+    provideClientHydration(withEventReplay()),
+    provideHttpClient()
   ],
   bootstrap: [AppComponent]
 })
--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Product } from '../../models/product';
+import { ProductService } from '../../services/product.service';
+
 
 
 @Component({
@@ -9,11 +11,20 @@ import { Product } from '../../models/product';
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit {
   products:Product[] = [];
+  dataLoaded = false;
+  apiUrl ="https://localhost:44366/api/products/getall";
 
-  constructor() { }
+  constructor(private productService:ProductService) { }
   ngOnInit() :void { 
+    this.getProducts();
   }
 
+  getProducts(){
+    this.productService.getProducts().subscribe(response => {
+      this.products =response.data
+      this.dataLoaded = true
+    })
+  }
 }
