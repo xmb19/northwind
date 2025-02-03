48f8dc8990f20489c639630d4b29a666d96d2314
xmb19 <muratbektas019@gmail.com>
2025-02-03 11:10:37 +0300

    03/02

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'; // FormsModule'u import edin
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
@@ -12,6 +13,9 @@ import { provideHttpClient } from '@angular/common/http';
 import { VatAddedPipe } from './pipes/vat-added.pipe';
 import { FilterPipePipe } from './pipes/filter-pipe.pipe';
 
+import { ToastrModule } from "ngx-toastr";
+import { CartSummaryComponent } from './components/cart-summary/cart-summary.component'
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -19,13 +23,17 @@ import { FilterPipePipe } from './pipes/filter-pipe.pipe';
     CategoryComponent,
     NaviComponent,
     VatAddedPipe,
-    FilterPipePipe
+    FilterPipePipe,
+    CartSummaryComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     FormsModule,
-    
+    ToastrModule.forRoot({
+      positionClass:"toast-bottom-right"
+    }),
   ],
   providers: [
     provideClientHydration(withEventReplay()),
--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -2,45 +2,60 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from '../../models/product';
 import { ProductService } from '../../services/product.service';
 import { ActivatedRoute } from '@angular/router';
-import { FormsModule } from '@angular/forms'; 
-
-
+import { FormsModule } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-product',
   standalone: false,
-  
+
   templateUrl: './product.component.html',
-  styleUrl: './product.component.css'
+  styleUrl: './product.component.css',
 })
 export class ProductComponent implements OnInit {
-  products:Product[] = [];
+  products: Product[] = [];
   dataLoaded = false;
-  filterText="";
+  filterText = '';
 
-  constructor(private productService:ProductService, private activatedRoute:ActivatedRoute) { }
+  constructor(
+    private productService: ProductService,
+    private activatedRoute: ActivatedRoute,
+    private toastrService: ToastrService,
+    private cartService: CartService,
+  ) {}
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe(params=>{
-      if(params["categoryId"]){
-        this.getProductsByCategory(params["categoryId"])
-      }else{
-        this.getProducts()
+    this.activatedRoute.params.subscribe((params) => {
+      if (params['categoryId']) {
+        this.getProductsByCategory(params['categoryId']);
+      } else {
+        this.getProducts();
       }
-    })
-}
-
-  getProducts(){
-    this.productService.getProducts().subscribe(response => {
-      this.products =response.data
-      this.dataLoaded = true
-    })
+    });
   }
 
-  getProductsByCategory(categoryId:number) {
-    this.productService.getProductsByCategory(categoryId).subscribe(response=>{
-      this.products = response.data
+  getProducts() {
+    this.productService.getProducts().subscribe((response) => {
+      this.products = response.data;
       this.dataLoaded = true;
-    })   
+    });
   }
 
+  getProductsByCategory(categoryId: number) {
+    this.productService
+      .getProductsByCategory(categoryId)
+      .subscribe((response) => {
+        this.products = response.data;
+        this.dataLoaded = true;
+      });
+  }
+
+  addToCart(product: Product) {
+    if (product.productId === 1) {
+      this.toastrService.error('Hata', 'Bu ürün sepete eklenemez!');
+    } else {
+      this.toastrService.success('Sepete eklendi', product.productName);
+      this.cartService.addToCart(product);
+    }
+  }
 }
